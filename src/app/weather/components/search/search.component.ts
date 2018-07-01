2448cf67439cb085a86134acea2068dca48623d1
Anthony Borisade <anthonyborisade@anthonys-mbp.home>
2018-07-01 15:57:41 +0100

    using dropdown

--- src/app/weather/components/search/search.component.ts
+++ src/app/weather/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Store } from '@ngrx/store'
 import { Component, OnInit, Input } from '@angular/core'
+import { FormControl } from '@angular/forms'
 import { Observable } from 'rxjs/Observable'
 import * as fromReducer from '../../store/reducers/weather'
 import * as fromActions from '../../store/actions/weather'
@@ -16,18 +17,33 @@ import { getWeather, getMessage } from '../../store/selectors/weather'
 export class SearchComponent implements OnInit {
   forecasts$: Observable<Forecast[]>
   message$: Observable<any>
+  selectedcity: FormControl
+  langs: string[] = [
+    'London',
+    'USA',
+    'Farborough',
+  ]
+
   @Input()
-  city ='London'
+  city = 'London'
+  lang = ''
+
   constructor(private store: Store<State>) {
     this.forecasts$ = this.store.select(getWeather)
 
   }
   ngOnInit() {
+    this.selectedcity = new FormControl()
 
   }
+  ChangeValue(event: Event): void {
+    console.log(event)
+    const value: string = (<HTMLSelectElement>event.srcElement).value
+    console.log(value)
+  }
+
   searchforecast(city: string) {
-    alert(this.city)
-    this.getforecastsbycity(city)
+    // this.getforecastsbycity(city)
   }
   getforecastsbycity(city) {
     this.store.dispatch(new fromActions.GetforecastsAction(city))
